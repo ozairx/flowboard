abebf68b47ddfa3eb96731eca8c50931d82d749d
Ozair Couto <zaicoutox@gmail.com>
2025-11-07 17:23:41 -0300

    chore: allow explicit "any"

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -9,6 +9,7 @@ export default tseslint.config(
   {
     rules: {
       '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
     },
   },
   prettierConfig,
