917cd243c0ad7c8a1af42e50aca0b5acc4951edd
Ozair Couto <zaicoutox@gmail.com>
2025-11-07 22:30:51 -0300

    fix: add body validation for middleware

--- backend/src/middlewares/authMiddleware.ts
+++ backend/src/middlewares/authMiddleware.ts
@@ -9,9 +9,15 @@ export const authMiddleware = (req: Request, res: Response, next: NextFunction) 
   }
 
   const token = authHeader.split(' ')[1];
+  if (!token) {
+    return res.status(401).json({ message: 'Non-existent token'});
+  }
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET || 'your_default_secret') as { id: number };
+    const decoded = jwt.verify(token, process.env.JWT_SECRET!) as any;
+    if (!decoded || !decoded.id) {
+      return res.status(401).json({ message: 'Invalid token payload' });
+    }
     req.user = decoded;
     next();
   } catch (error) {
