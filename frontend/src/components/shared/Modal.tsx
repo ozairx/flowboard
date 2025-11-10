40e738eef33f492c26193efa558b2fc45f894f72
Ozair Couto <zaicoutox@gmail.com>
2025-11-10 10:12:43 -0300

    feat: create and use ui components

---
+++ frontend/src/components/shared/Modal.tsx
@@ -0,0 +1,27 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+interface ModalProps {
+  trigger: React.ReactNode;
+  title: string;
+  children: React.ReactNode;
+}
+
+export default function Modal({ trigger, title, children }: ModalProps) {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>{trigger}</DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+        </DialogHeader>
+        {children}
+      </DialogContent>
+    </Dialog>
+  );
+}
