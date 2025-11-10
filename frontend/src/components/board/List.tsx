7483bf395dc2d0678b65782e2285aaede7e59565
Ozair Couto <zaicoutox@gmail.com>
2025-11-10 13:31:29 -0300

    feat: add suport for drag'n'drop

--- frontend/src/components/board/List.tsx
+++ frontend/src/components/board/List.tsx
@@ -1,5 +1,6 @@
 import Card from './Card';
 import { Card as ShadcnCard, CardHeader, CardTitle, CardContent } from '@/components/ui/card';
+import { Droppable, Draggable } from '@hello-pangea/dnd';
 
 interface ListProps {
   list: {
@@ -8,6 +9,7 @@ interface ListProps {
     cards: {
       id: string;
       title: string;
+      order: number;
     }[];
   };
 }
@@ -18,11 +20,30 @@ export default function List({ list }: ListProps) {
       <CardHeader className="p-2">
         <CardTitle className="text-base">{list.title}</CardTitle>
       </CardHeader>
-      <CardContent className="p-2">
-        {list.cards.map((card) => (
-          <Card key={card.id} card={card} />
-        ))}
-      </CardContent>
+      <Droppable droppableId={list.id} type="card">
+        {(provided) => (
+          <CardContent
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+            className="p-2"
+          >
+            {list.cards.map((card, index) => (
+              <Draggable key={card.id} draggableId={card.id} index={index}>
+                {(provided) => (
+                  <div
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                    ref={provided.innerRef}
+                  >
+                    <Card card={card} />
+                  </div>
+                )}
+              </Draggable>
+            ))}
+            {provided.placeholder}
+          </CardContent>
+        )}
+      </Droppable>
     </ShadcnCard>
   );
 }
