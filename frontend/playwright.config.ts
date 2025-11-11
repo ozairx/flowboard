4d2d2d0ded8dc8325536f38bc67f07a0f425552d
Ozair Couto <zaicoutox@gmail.com>
2025-11-11 11:59:17 -0300

    test: setup Playwright

---
+++ frontend/playwright.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  timeout: 60000, // 60 seconds
+  use: {
+    baseURL: 'http://localhost:3000',
+  },
+  webServer: {
+    command: 'npm run dev',
+    url: 'http://localhost:3000',
+    reuseExistingServer: !process.env.CI,
+  },
+});
